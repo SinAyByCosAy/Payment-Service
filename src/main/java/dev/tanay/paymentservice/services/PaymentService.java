5f97cdf9dd59e8cede4e7ee9b255a78e0265c47d
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-05-11 01:29:19 +0530

    Payment service: get amount

--- src/main/java/dev/tanay/paymentservice/services/PaymentService.java
+++ src/main/java/dev/tanay/paymentservice/services/PaymentService.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class PaymentService {
     public String initiatePayment(String orderId){
+        // assuming here we make a call to Order Service(separate service) to get Order details for the orderId
+        // Order order = orderService.getOrderDetails(orderId);
+        // Long amt = order.getAmount();
+        Long amount = 1010L;
         return "Hello: " + orderId;
     }
 }
