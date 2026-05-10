d43bef1faa0a9abc2639846e102564ba0c52d45d
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-05-11 00:44:55 +0530

    Payment controller setup

---
+++ src/main/java/dev/tanay/paymentservice/controllers/PaymentController.java
@@ -0,0 +1,12 @@
+package dev.tanay.paymentservice.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+    @PostMapping("/{orderId}")
+    public String initiatePayment(@PathVariable String orderId){
+        return "Hello: " + orderId;
+    }
+}
