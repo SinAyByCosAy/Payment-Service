5d45496d2559772c18af6cec2a44c89505f2eda8
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-05-11 01:56:39 +0530

    Payment Gateway chooser: setup

--- src/main/java/dev/tanay/paymentservice/paymentgateway/RazorpayPaymentGateway.java
+++ src/main/java/dev/tanay/paymentservice/paymentgateway/RazorpayPaymentGateway.java
@@ -1,6 +1,10 @@
 package dev.tanay.paymentservice.paymentgateway;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RazorpayPaymentGateway implements PaymentGateway{
+    @Override
     public String generatePaymentLink(){
 
     }
--- src/main/java/dev/tanay/paymentservice/paymentgateway/StripePaymentGateway.java
+++ src/main/java/dev/tanay/paymentservice/paymentgateway/StripePaymentGateway.java
@@ -1,6 +1,10 @@
 package dev.tanay.paymentservice.paymentgateway;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class StripePaymentGateway implements PaymentGateway{
+    @Override
     public String generatePaymentLink(){
 
     }
