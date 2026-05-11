cfd971b5db5c66c00dc56b6c4d37f64437b7a2ea
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-05-12 03:19:53 +0530

    Payment Controller added

---
+++ src/main/java/dev/tanay/paymentservice/controllers/PaymentController.java
@@ -0,0 +1,16 @@
+package dev.tanay.paymentservice.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+
+    @PostMapping("/")
+    public String initiatePayment(@RequestBody){
+        return null;
+    }
+}
