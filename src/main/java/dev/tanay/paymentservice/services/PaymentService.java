605f7f673e53bda1a6b19d0d5788712d4ce1d1d8
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-05-12 13:07:17 +0530

    Payment service and chooser: link established

--- src/main/java/dev/tanay/paymentservice/services/PaymentService.java
+++ src/main/java/dev/tanay/paymentservice/services/PaymentService.java
@@ -1,5 +1,7 @@
 package dev.tanay.paymentservice.services;
 
+import dev.tanay.paymentservice.services.paymentgateway.PaymentGateway;
+import dev.tanay.paymentservice.services.paymentgateway.PaymentGatewayChooserStrategy;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,7 +10,13 @@ public class PaymentService {
     // then select the best available Payment Gateway
     // make a call to PG to get the link for the payment
     // return back the payment link
+
+    private PaymentGatewayChooserStrategy paymentGatewayChooserStrategy;
+    public PaymentService(PaymentGatewayChooserStrategy paymentGatewayChooserStrategy){
+        this.paymentGatewayChooserStrategy = paymentGatewayChooserStrategy;
+    }
     public String initiatePayment(String email, String phoneNo, String orderId, Long amount){
+        PaymentGateway pg = paymentGatewayChooserStrategy.getBestPaymentGateway();
         return null;
     }
 }
