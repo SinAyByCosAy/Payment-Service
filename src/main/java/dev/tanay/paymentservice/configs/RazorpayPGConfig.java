3017483562216225862e7f24de710b1c3720ab6e
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-05-12 13:14:21 +0530

    Razorpay config: added

---
+++ src/main/java/dev/tanay/paymentservice/configs/RazorpayPGConfig.java
@@ -0,0 +1,19 @@
+package dev.tanay.paymentservice.configs;
+
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RazorpayPGConfig {
+    @Value("${razorpay.key.id}")
+    private String razorpayKeyId;
+    @Value("${razorpay.secret.key}")
+    private String razorpaySecretKey;
+    @Bean
+    public RazorpayClient initializePaymentGateway() throws RazorpayException {
+        return new RazorpayClient(razorpayKeyId, razorpaySecretKey);
+    }
+}
