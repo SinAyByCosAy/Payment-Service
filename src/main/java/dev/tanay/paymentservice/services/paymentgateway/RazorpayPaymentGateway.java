95c7862bd80449cf478b989e21c3275bccd0c702
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-05-12 12:52:58 +0530

    Payment gateway chooser: strategy

--- src/main/java/dev/tanay/paymentservice/services/paymentgateway/RazorpayPaymentGateway.java
+++ src/main/java/dev/tanay/paymentservice/services/paymentgateway/RazorpayPaymentGateway.java
@@ -1,5 +1,8 @@
 package dev.tanay.paymentservice.services.paymentgateway;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RazorpayPaymentGateway implements PaymentGateway{
     @Override
     public String generatePaymentLink(String email, String phoneNo, String orderId, Long amount){
--- src/main/java/dev/tanay/paymentservice/services/paymentgateway/StripePaymentGateway.java
+++ src/main/java/dev/tanay/paymentservice/services/paymentgateway/StripePaymentGateway.java
@@ -1,6 +1,9 @@
 package dev.tanay.paymentservice.services.paymentgateway;
 
-public class StripePaymentLink implements PaymentGateway{
+import org.springframework.stereotype.Service;
+
+@Service
+public class StripePaymentGateway implements PaymentGateway{
     @Override
     public String generatePaymentLink(String email, String phoneNo, String orderId, Long amount){
         return null;
